f8dc285d75e0eac970e0e3d62b4dc23413e224bf
Oscar Sanchez <oscar99emilios@gmail.com>
2022-10-06 23:47:24 -0500

    -r requiremets

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
