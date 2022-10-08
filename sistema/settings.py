96faaec72a4df9b7924e146442a4b66ebf7e9c67
oscar99sanchez <104732084+oscar99sanchez@users.noreply.github.com>
2022-10-07 23:23:13 -0500

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = []
 
 
