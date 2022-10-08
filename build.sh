65a1b851c7812539b86711d4e3a57c4c1c762290
Oscar Sanchez <oscar99emilios@gmail.com>
2022-10-08 01:44:11 -0500

    -r requiremets

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py shell /libreria/admin.py
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('oscar123', 'userEmail', '123')" | python manage.py shell
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
--- libreria/admin.py
+++ libreria/admin.py
@@ -2,7 +2,3 @@ from django.contrib import admin
 from .models import Mouses
 # Register your models here.
 admin.site.register(Mouses)
-
-from django.contrib.auth import get_user_model
-User = get_user_model()
-User.objects.create_superuser('lol', 'dev@email.io', 'password')
